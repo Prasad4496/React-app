4ba93131be0638ca620dd4608de34c36e64924ef
Prasad4496 <prasadlokhande2@gmail.com>
2022-06-08 13:28:07 +0530

    Updates

---
+++ precache-manifest.c63746b146147f50e965ebcfed125319.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3b23e07725292dc33e6",
+    "url": "/React-app/static/css/main.a5fba9fb.chunk.css"
+  },
+  {
+    "revision": "f3b23e07725292dc33e6",
+    "url": "/React-app/static/js/main.f3b23e07.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/React-app/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/React-app/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "97dcc9de5bb8fe9bcc09",
+    "url": "/React-app/static/js/runtime~main.97dcc9de.js"
+  },
+  {
+    "revision": "dc3cb2899740153017f97648e7d20319",
+    "url": "/React-app/index.html"
+  }
+];
